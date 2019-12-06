91982ead7662ef005b2460e1787a6a4b23e5ecfa
abinjomathew <abinjomathew@gmail.com>
2019-12-06 05:56:16 +0530

    loading....

--- app/src/main/java/com/example/work1/MainActivity.java
+++ app/src/main/java/com/example/work1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.work1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
